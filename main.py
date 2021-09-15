ca57f3aa4c18041a7cb45d2f9e9dd11a703f9962
ansar-95 <60046679+ansar-95@users.noreply.github.com>
2021-09-15 17:11:35 +0200

    correction

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-def calculDateDuJour(date):
-
+def calculDateDuJour():
+    dateInput = input('Saisir une date au format jj/mm/aaaa')
+    date = dateInput.split('/')
     if len(date) != 3 or len(date[0]) > 3 or len(date[1]) > 3 or len(date[2]) > 5 or int(date[2]) < 1583 or int(date[2]) > 9999:
-        saisieDate()
+        return calculDateDuJour()
     else:
         c = (14 - int(date[1])) // 12
         a = int(date[2]) - c
         m = int(date[1]) + 12 * c - 2
-        j = (int(date[0]) + a + a / 4 - a / 100 + a / 400 + (31 * m) // 12) % 7
-        return round(j)
+        j = (int(date[0]) + a + a // 4 - a // 100 + a // 400 + (31 * m) // 12) %7
+        return jourDeLaSemaine(j)
 def jourDeLaSemaine(index):
-    jours = {0 : "lundi", 1 : "mardi", 2 : "mercredi", 3 : "jeudi", 4 : "vendredi", 5 : "samedi", 6 : "dimanche"}
-    return jours[index -1]
-
-def saisieDate():
-    dateInput = input('Saisir une date au format jj/mm/aaaa')
-    return jourDeLaSemaine(calculDateDuJour(dateInput.split('/')))
-
-print(saisieDate())
+    jours = {1 : "lundi", 2 : "mardi", 3 : "mercredi", 4 : "jeudi", 5 : "vendredi", 6 : "samedi", 0 : "dimanche"}
+    return jours[index]
+print(calculDateDuJour())
